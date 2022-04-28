79f4504f2f01438e69e1cbc75ea7a22983c976dc
Goblin <jackmord445@gmail.com>
2022-04-28 09:18:05 +0000

    define props and basic structure

---
+++ src/components/ChatComponents/SideBar/components/ChannelInfo.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+const props = defineProps({
+    channelName: {
+        type: String,
+        default: "Unkown Channel"
+    },
+    channelDescription: {
+        type: String,
+        default: "Channel has no description"
+    },
+
+    channelMembersList: {
+        type: Array,
+        default() {
+            return []
+        }
+    }
+})
+
+</script>
+
+<template>
+    <div class="ChannelName">
+        {{props.channelName}}
+    </div>
+
+    <div class="ChannelDescription">
+        {{ props.channelDescription }}
+    </div>
+
+    <div>
+        <span>Members</span>
+
+        <!-- members list here -->
+        <p>Members list here</p>
+    </div>
+
+</template>
