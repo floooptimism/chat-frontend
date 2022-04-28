cfe2f8f1ce598db6c88703886d1f9bed677443b1
Goblin <jackmord445@gmail.com>
2022-04-28 09:18:37 +0000

    basic structure

---
+++ src/components/ChatComponents/SideBar/components/ChannelTopBar.vue
@@ -0,0 +1,29 @@
+<script setup>
+const emit = defineEmits(["eventGoBack"])
+</script>
+
+<template>
+  <div class="cursor-pointer flex items-center" @click="emit('eventGoBack')">
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="22"
+        height="22"
+        viewBox="0 0 512 512"
+      >
+        <title>ionicons-v5-a</title>
+        <polyline
+          points="328 112 184 256 328 400"
+          style="
+            fill: none;
+            stroke: #fff;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+            stroke-width: 48px;
+          "
+        />
+      </svg>
+    </div>
+    <h1 class="ml-2">All Channels</h1>
+  </div>
+</template>
