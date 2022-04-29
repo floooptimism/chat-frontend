ba41e6636576ae68d44bbdc67c50c361cbc5f6d7
Goblin <jackmord445@gmail.com>
2022-04-29 16:17:43 +0000

    finalized

--- src/components/ChatComponents/SideBar/components/ChannelInfo.vue
+++ src/components/ChatComponents/SideBar/components/ChannelInfo.vue
@@ -1,39 +1,71 @@
 <script setup>
+import { ref } from "vue";
+import { faker } from "@faker-js/faker";
+import EntityLabel from "../../../EntityLabel/EntityLabel.vue";
 
 const props = defineProps({
-    channelName: {
-        type: String,
-        default: "Unkown Channel"
-    },
-    channelDescription: {
-        type: String,
-        default: "Channel has no description"
-    },
+  channelName: {
+    type: String,
+    default: "Unkown Channel",
+  },
+  channelDescription: {
+    type: String,
+    default: "Channel has no description",
+  },
 
-    channelMembersList: {
-        type: Array,
-        default() {
-            return []
-        }
-    }
-})
+  channelMembersList: {
+    type: Array,
+    default() {
+      return [];
+    },
+  },
+});
 
+const channelMembersList = ref(props.channelMembersList);
 </script>
 
 <template>
-    <div class="ChannelName">
-        {{props.channelName}}
+  <div class="ChannelInfo">
+    <h1 class="ChannelName">
+      {{ props.channelName }}
+    </h1>
+
+    <p class="ChannelDescription">
+      {{ props.channelDescription }}
+    </p>
+
+    <div class="MembersContainer">
+      <h1 class="MembersHeader">Members</h1>
+      <EntityLabel
+        v-for="member in channelMembersList"
+        :entityName="member.name"
+        :key="member.id"
+        :entityIdentifier="member.id"
+      />
     </div>
-
-    <div class="ChannelDescription">
-        {{ props.channelDescription }}
-    </div>
-
-    <div>
-        <span>Members</span>
-
-        <!-- members list here -->
-        <p>Members list here</p>
-    </div>
-
+  </div>
 </template>
+
+<style scoped>
+.ChannelInfo {
+  color: var(--text-color);
+  @apply flex flex-col;
+  @apply mt-4;
+}
+
+.ChannelName {
+  @apply text-xl font-medium tracking-wide;
+}
+
+.ChannelDescription {
+  @apply mt-3;
+}
+
+.MembersContainer {
+  @apply mt-6;
+}
+
+.MembersHeader {
+  @apply text-xl font-medium tracking-wide;
+}
+</style>
