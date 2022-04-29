869575e6344458e6e85451898a7ef043a5fc6518
Goblin <jackmord445@gmail.com>
2022-04-29 16:17:18 +0000

    Entity label

---
+++ src/components/EntityLabel/EntityLabel.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+const props = defineProps({
+    imageSrc: String,
+    entityName: {
+        type: String,
+        default: "Unknown entity"
+    },
+    entityIdentifier: String
+})
+
+const emit = defineEmits(["eventEntityClicked"]);
+
+</script>
+
+<template>
+    <div class="Entity" @click="emit('eventEntityClicked')">
+        <div class="ImageContainer">
+            <img :src="props.imageSrc" alt="Channel Image" />
+        </div>
+
+        <div class="ml-4 font-medium">
+            {{ props.entityName }}
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .Entity {
+        @apply flex items-center;
+        @apply py-3;
+    }
+
+    .ImageContainer {
+        @apply w-8 h-8;
+        @apply overflow-hidden;
+        @apply rounded-md;
+    }
+
+    .ImageContainer img {
+        width: 100%;
+        height: 100%;
+    }
+
+</style>
