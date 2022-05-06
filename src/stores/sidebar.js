b831e640562c5e1e715d18eae53c55938e65e53b
goblin <jackmord445@gmail.com>
2022-05-06 16:00:40 +0800

    sidebar store

---
+++ src/stores/sidebar.js
@@ -0,0 +1,24 @@
+import { defineStore } from "pinia";
+
+
+const useSideBarStore = defineStore('sidebar', {
+    states: {
+        isOpen: false,
+        isDisplayingChannels: false
+    },
+
+    actions: {
+        toggleSideBar() {
+            this.isOpen = !this.isOpen;
+        },
+        toggleIsDisplayingChannels() {
+            this.isDisplayingChannels = !this.isDisplayingChannels;
+        },
+        setIsDisplayingChannels(param){
+            this.isDisplayingChannels = param;
+        }
+    }
+    
+});
+
+export default useSideBarStore;
