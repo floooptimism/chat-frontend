dc9a3f0c2b9a0e7699b410c0438f566fde50d98f
goblin <jackmord445@gmail.com>
2022-05-06 16:03:44 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,20 @@
 import NavBar from './components/ChatComponents/NavBar/NavBar.vue';
 import SideBar from './components/ChatComponents/SideBar/SideBar.vue';
 
-import {ref} from 'vue';
-
-const sideBarOpen = ref(false);
-
-const eventHandlers = {
-  toggleSideBar(){
-    sideBarOpen.value = !sideBarOpen.value;
-  }
-}
-
-function toggleSideBar(){
-  
-}
+import { ref } from 'vue';
+import ChatArea from './components/ChatComponents/ChatArea/ChatArea.vue';
 
 </script>
 
 <template>
   <div>
-    <NavBar @eventKebabClick="eventHandlers.toggleSideBar()"/>
-    <SideBar @eventToggleSideBar="eventHandlers.toggleSideBar()" :isSideBarOpen="sideBarOpen" />
+    <SideBar/>
+    <NavBar />
+    <ChatArea />
   </div>
 </template>
 
 <style>
-
 .notest {
   @apply text-red-500;
 }
