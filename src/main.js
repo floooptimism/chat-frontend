e2ec90c8a607f08e802fd1e6181349e8be97cec2
goblin <jackmord445@gmail.com>
2022-05-06 16:03:29 +0800

    stores

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createPinia } from 'pinia';
 import './global.css';
+import useChannels from './stores/channel';
+
+// Vue Plugins
+import { createPinia } from 'pinia';
+
+// devtools plugin
+// import FlashUpdates from 'vue-devtool-flash-updates';
 
 
 const app = createApp(App);
 
 app.use(createPinia());
+// app.use(FlashUpdates);
+
+window.store = {};
+window.store.channels = useChannels();
+
 
 app.mount('#app');
