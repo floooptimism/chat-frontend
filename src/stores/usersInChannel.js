9dfbe7323c6bcbec1889337b3254506ade2f2d18
goblin <jackmord445@gmail.com>
2022-05-22 00:30:23 +0800

    users store

---
+++ src/stores/usersInChannel.js
@@ -0,0 +1,18 @@
+import { defineStore } from "pinia";
+
+
+const useUsersInChannel = defineStore("usersInChannel", {
+    state: () => {
+        return {
+            users: [],
+        }
+    },
+
+    actions: {
+        updateUsers(users){
+            this.users = users;
+        }
+    }
+});
+
+export default useUsersInChannel;
