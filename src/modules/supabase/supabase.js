6e418fce58b4b5392345e65269310d4acdff4129
goblin <jackmord445@gmail.com>
2022-05-23 01:42:43 +0800

    auth store, integrate to supabase

---
+++ src/modules/supabase/supabase.js
@@ -0,0 +1,29 @@
+import { createClient } from "@supabase/supabase-js";
+import useAuth from "../../stores/auth";
+import pinia from "../../stores/piniaInstance";
+
+const supabase = createClient(
+  "https://dzcqwycbvybucsdkrevg.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6Y3F3eWNidnlidWNzZGtyZXZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDk5MjExMzEsImV4cCI6MTk2NTQ5NzEzMX0.7rVGq2Po4c1uxZjNWyFQaAOaZadgnxRhjXofm9_Sc80",
+  {
+    persistSession: true,
+    detectSessionInUrl: true
+  }
+);
+
+export function initSupaBase() {
+  let authStore = useAuth(pinia);
+  supabase.auth.onAuthStateChange((event, session) => {
+    console.log(event);
+    if (session) {
+      authStore.setAuth(true);
+      authStore.setToken(session.provider_token);
+    } else {
+      authStore.setAuth(false);
+      authStore.setToken(null);
+    }
+  });
+}
+
+
+export default supabase;
---
+++ src/stores/auth.js
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia";
+
+
+const useAuth = defineStore("auth", {
+    state: () => {
+        return {
+            authenticated: false,
+            token: "",
+        }
+    },
+
+    actions: {
+        setAuth(auth){
+            this.authenticated = auth;
+        },
+        setToken(token){
+            this.token = token;
+        }
+    }
+});
+
+export default useAuth;
