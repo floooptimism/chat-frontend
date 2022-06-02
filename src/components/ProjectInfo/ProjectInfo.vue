82d5ea7cdffc2389050a25fcc1d6c281d7b27ef3
goblin <jackmord445@gmail.com>
2022-06-02 22:58:37 +0800

    responsive

--- src/components/ProjectInfo/ProjectInfo.vue
+++ src/components/ProjectInfo/ProjectInfo.vue
@@ -14,8 +14,8 @@ const eventHandlers = {
 <template>
     <vue-final-modal v-model="showModal" :click-to-close="false">
         <div class="Notify">
-            <h1 class="text-2xl mb-4 font-medium">Current project limitations / WIP</h1>
-            <ul class="list-disc pl-8 text-sm flex flex-col space-y-2">
+            <h1 class="text-xl md:text-2xl mb-4 font-medium">Current project limitations / WIP</h1>
+            <ul class="list-disc pl-8 text-xs md:text-sm flex flex-col space-y-2">
                 <li>Current iteration doesn't have data persistence.</li>
                 <li>Max messages you can view is 100 messages, old messages get replaced by new once limit goes past 100.</li>
                 <li>You can't see active users in a channel.</li>
@@ -30,7 +30,7 @@ const eventHandlers = {
 
 <style scoped>
     .Notify{
-        @apply absolute -translate-x-1/2 py-8 px-8 top-24 left-1/2;
+        @apply absolute w-5/6 -translate-x-1/2 py-8 px-8 top-24 left-1/2;
         @apply break-words;
         background-color: var(--primary-color);
         color: var(--text-color);
@@ -38,7 +38,7 @@ const eventHandlers = {
     }
 
     .hideModalButton {
-        @apply mt-8 px-2 py-1 rounded;
+        @apply mt-8 text-sm md:text-base px-2 py-1 rounded;
         @apply mx-auto;
         background-color: #42b883;
         color: black;
